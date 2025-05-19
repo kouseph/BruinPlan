f945296d56fbf82331881e871c5797ec1b58881c
EthanLee101 <eplee24@g.ucla.edu>
2025-05-18 19:10:55 -0700

    basic google oauth

---
+++ backend/auth.js
@@ -0,0 +1,26 @@
+import passport from 'passport';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
+
+
+passport.use(new GoogleStrategy({
+    clientID:     process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/google/callback",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+  }
+));
+
+passport.serializeUser(function(user,done){
+    done(null,user);
+});
+
+passport.deserializeUser(function(user,done){
+    done(null,user);
+});
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,61 @@
 // index.js
-import http from "http";
+import http, { ServerResponse } from "http";
+import express from "express";
+import session from "express-session";
+import './auth.js'
+import { appendFile } from "fs";
+import passport from "passport";
 
 const hostname = "127.0.0.1"; // or 'localhost'
 const port = 3000;
+const server = express();
 
-const server = http.createServer((req, res) => {
+server.use(session({secret: process.env.SECRET})); 
+server.use(passport.initialize());
+server.use(passport.session());
+
+function isLoggedIn(req,res,next){
+    req.user ? next() : res.sendStatus(401);
+}
+server.get('/', (req, res) => {
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
+
+server.get('/auth/google',
+  passport.authenticate('google', { scope: ['email','profile']})
+)
+
+server.get('/google/callback',
+  passport.authenticate('google',{
+    successRedirect: '/protected',
+    failureRedirect: '/auth/failure',
+  })
+)
+
+server.get('/auth/google', (req, res) => {
   res.statusCode = 200; // HTTP status OK
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello from Node.js!\n");
+  res.send('You clicked the Google Auth link!');
+});
+
+server.get('/protected', isLoggedIn, (req,res) => { // route once logged in
+  res.send("Success");
+})
+
+server.get('/auth/failure', (req,res) => { // route once logged in
+  res.send("Something Went Wrong");
+})
+
+server.get('/logout', (req, res, next) => {
+  req.logout(function(err) {
+    if (err) return next(err);
+    req.session.destroy(() => {
+      res.redirect('/');
+    });
+  });
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
